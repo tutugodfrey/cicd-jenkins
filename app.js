0a07cb6adcd32157d4b041bfa85fb4133916b47f
Godfrey Tutu <godfreytutu@Godfreys-MacBook-Pro.local>
2019-07-05 11:04:35 +0100

    export app and update how supertest is used

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 4000
 app.get('/',function(req,res) {
   res.send("Hello World! Jenkins is integrated!");
 });
 
-app.listen(4000);
+app.listen(port);
+
+module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 var supertest = require("supertest");
 var should = require("should");
+var app = require('../app');
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://localhost:4000");
+// var server = supertest.agent("http://localhost:4000");
 
 // UNIT test begin
 
@@ -12,7 +13,7 @@ describe("SAMPLE unit test",function(){
   // #1 should return home page
   it("should return home page",function(done){
     // calling home page
-    server
+    supertest(app)
     .get("/")
     .expect("Content-type",/text/)
     .expect(200) // THis is HTTP response
